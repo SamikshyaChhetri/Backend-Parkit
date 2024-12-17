4011f48cccd72955438b8b84a961dd3f7e4d4c4b
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-17 23:14:57 +0545

    get and post request implemented

--- index.js
+++ index.js
@@ -3,25 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 const prisma = new PrismaClient();
-// app.get("/path1", (req, res) => {
-//   console.log(req);
-//   res.status(400).send("This is response from get request");
-//   return;
-// });
-// app.post("/path2", (req, res) => {
-//   console.log(req.body);
-//   res.send(`Your name is ${req.body.name}`);
-//   if (!req.body.num1) {
-//     return res.status(400).send("Num1 is required");
-//   }
-//   if (!req.body.num2) {
-//     return res.status(400).send("Num2 is required");
-//   }
-//   const sum = req.body.num1 + req.body.num2;
-//   return res.send({
-//     sum,
-//   });
-// });
+
 app.post("/adduser", async (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
