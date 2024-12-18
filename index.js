d80e280888634628d643630b195c7dad32531045
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-18 17:59:32 +0545

    CRUD completed

--- index.js
+++ index.js
@@ -9,20 +9,82 @@ app.post("/adduser", async (req, res) => {
   const email = req.body.email;
   const id = Date.now().toString();
   const age = req.body.age;
+  const gender = req.body.gender;
   const createUser = await prisma.user.create({
     data: {
       name,
       email,
       id,
       age,
+      gender,
     },
   });
-  res.send(createUser);
+  return res.send(createUser);
 });
 app.get("/getuser", async (req, res) => {
   const alluser = await prisma.user.findMany();
   res.send(alluser);
 });
+app.get("/getsingleuser/:email", async (req, res) => {
+  console.log(req.params);
+  const user = await prisma.user.findFirst({
+    where: {
+      email: req.params.email,
+    },
+  });
+  if (!user) {
+    return res.send("No user found");
+  }
+
+  return res.send(user);
+});
+
+app.delete("/deleteuser/:id", async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (user) {
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.send(deletedUser);
+  }
+  return res.send("User not found");
+});
+app.patch("/updateUser/:id", async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (user) {
+    const { name, email, age, gender } = req.body;
+    const updateuserData = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      // data: {
+      //   name: req.body.name,
+      //   email: req.body.name,
+      //   age: req.body.age,
+      //   gender:req.body.gender,
+      // },
+      data: {
+        name,
+        email,
+        age,
+        gender,
+      },
+    });
+    return res.send(updateuserData);
+  }
+  return res.send("No data available");
+});
+
 app.listen(3333, () => {
   console.log("Server is running at 3333 port");
 });
