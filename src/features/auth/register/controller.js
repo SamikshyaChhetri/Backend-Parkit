8c31e7911c4a4f364dd207af0dfab49614937eff
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-20 22:04:25 +0545

    data stored in db on registering

--- src/features/auth/register/controller.js
+++ src/features/auth/register/controller.js
@@ -3,14 +3,15 @@ const prisma = new PrismaClient();
 export const registerController = async (req, res) => {
   const name = req.body.username;
   const email = req.body.email;
-  const password = req.body.password;
+  // const password = req.body.password;
   const phone = req.body.phone;
   const address = req.body.address;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
   const createUser = await prisma.user.create({
     data: {
       name,
       email,
-      password,
+      hashedPassword,
       address,
       phone,
     },
