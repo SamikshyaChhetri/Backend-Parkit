527ab9e8d2111fdce7531b0fba474adb93ee3934
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-21 21:57:29 +0545

    login and register form completed

--- src/features/auth/login/controller.js
+++ src/features/auth/login/controller.js
@@ -3,18 +3,33 @@ import { prisma } from "../register/controller.js";
 export const loginController = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email, password);
+  // console.log(email, password);
   const findUser = await prisma.user.findFirst({
     where: {
       email,
     },
   });
   if (!findUser) {
-    return res.send("User not found");
+    return res.status(404).send({
+      success: false,
+      data: [],
+      message: "User not found",
+      error: [],
+    });
   }
   const comparePassword = await bcrypt.compare(password, findUser.password);
   console.log(comparePassword);
   comparePassword
-    ? res.status(200).send("User logged in")
-    : res.status(401).send("Incorrect password");
+    ? res.status(200).send({
+        success: true,
+        data: [],
+        message: "User logged in successfully",
+        error: [],
+      })
+    : res.status(401).send({
+        success: false,
+        data: [],
+        message: "Invalid password",
+        error: [],
+      });
 };
--- src/features/auth/register/controller.js
+++ src/features/auth/register/controller.js
@@ -4,10 +4,23 @@ export const prisma = new PrismaClient();
 export const registerController = async (req, res) => {
   const name = req.body.username;
   const email = req.body.email;
-  // const password = req.body.password;
   const phone = req.body.phone;
   const address = req.body.address;
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const findEmail = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+  if (findEmail) {
+    return res.status(403).send({
+      success: false,
+      data: [],
+      message: "Email already exists",
+      error: [],
+    });
+  }
+
   const createUser = await prisma.user.create({
     data: {
       name,
@@ -17,5 +30,10 @@ export const registerController = async (req, res) => {
       phone,
     },
   });
-  return res.send(createUser);
+  return res.send({
+    success: true,
+    data: createUser,
+    message: "User registered successfully",
+    error: [],
+  });
 };
