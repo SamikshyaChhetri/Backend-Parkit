f2454e59e29179aa9c5925e9f505a77d6b592b88
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-28 17:58:37 +0545

    avatar generator function added

---
+++ src/utils/utils.js
@@ -0,0 +1,62 @@
+const seeds = [
+  "oceanic_wave",
+  "firefly_dusk",
+  "lunar_dream",
+  "nebula_glow",
+  "stellar_breeze",
+  "comet_dash",
+  "twilight_haven",
+  "crystal_ridge",
+  "aurora_flare",
+  "zenith_echo",
+  "canyon_spark",
+  "forest_bloom",
+  "horizon_fade",
+  "midnight_blaze",
+  "sunrise_glint",
+  "pixel_mage",
+  "cyber_wisp",
+  "etheric_blush",
+  "harmony_glide",
+  "radiant_shore",
+  "vivid_orbit",
+  "shadow_wisp",
+  "galaxy_surge",
+  "cosmic_ray",
+  "nebula_frost",
+  "nova_pulse",
+  "eclipse_haze",
+  "driftwood_soul",
+  "cascade_vault",
+  "thunder_bloom",
+  "ember_twist",
+  "frost_spark",
+  "verdant_whisper",
+  "mystic_vibe",
+  "shimmer_strike",
+  "tidal_flight",
+  "astral_ripple",
+  "flame_jewel",
+  "quartz_gale",
+  "jade_spectrum",
+  "aurora_dusk",
+  "orbit_shift",
+  "luminous_arc",
+  "serenity_tide",
+  "blaze_flicker",
+  "prismatic_gleam",
+  "celestial_glint",
+  "ember_horizon",
+  "velvet_wisp",
+  "chrono_glow",
+];
+
+export const generateAvatar = () => {
+  const randomNumber = Math.floor(Math.random() * 49);
+  //   console.log(randomNumber);
+
+  const layout =
+    "https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=" +
+    seeds[randomNumber];
+  return layout;
+};
