6091283699f0100ed435a2aa99ff322cf5222965
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-30 21:13:42 +0545

    new path added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import loginrouter from "./src/features/auth/login/routes.js";
 import registerRouter from "./src/features/auth/register/route.js";
+import listRouter from "./src/features/listing/route.js";
 import userRouter from "./src/features/users/route.js";
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/auth", registerRouter);
 app.use("/users", userRouter);
 app.use("/auth", loginrouter);
+app.use("/listing", listRouter);
 app.listen(3333, () => {
   console.log("Server is running at 3333 port");
 });
