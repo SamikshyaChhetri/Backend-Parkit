fb14500b0e6537d4080427bfd5a62ea47ce35bb3
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-12-31 22:39:59 +0545

    controller and route added for listings

--- src/features/listing/controller.js
+++ src/features/listing/controller.js
@@ -1,21 +1,8 @@
 import { prisma } from "../auth/register/controller.js";
 
-export const listController = async (req, res) => {
-  const {
-    city,
-    street,
-    country,
-    zipcode,
-    type,
-    description,
-    rating,
-    price,
-    no_of_vehicle,
-    ownerId,
-  } = req.body;
-
-  const createList = await prisma.listing.create({
-    data: {
+export const createListingController = async (req, res) => {
+  try {
+    const {
       city,
       street,
       country,
@@ -26,25 +13,64 @@ export const listController = async (req, res) => {
       price,
       no_of_vehicle,
       ownerId,
-    },
-  });
+    } = req.body;
 
-  return res.send({
+    const createList = await prisma.listing.create({
+      data: {
+        city,
+        street,
+        country,
+        zipcode,
+        type,
+        description,
+        rating,
+        price,
+        no_of_vehicle,
+        ownerId,
+      },
+    });
+
+    return res.status(201).send({
+      success: true,
+      data: createList,
+      message: "List added successfully",
+      error: [],
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      data: [],
+      message: "Internal server error",
+      error: err,
+    });
+  }
+};
+export const getlistingsController = async (req, res) => {
+  const all_list = await prisma.listing.findMany();
+  return res.status(200).send({
     success: true,
-    data: createList,
-    message: "list added successfully",
+    data: all_list,
+    message: "All Listing retrived",
     error: [],
   });
 };
 
-export const getlistController = async (req, res) => {
-  const all_list = await prisma.listing.findMany();
-  res.send(all_list);
-};
-
-export const getSingleList = async (req, res) => {
+export const getSingleListing = async (req, res) => {
   const listdata = await prisma.listing.findFirst({
     where: { id: req.params.id },
   });
-  return res.send(getSingleList);
+  if (listdata) {
+    return res.status(200).send({
+      success: true,
+      data: listdata,
+      message: "Single listing retrieved",
+      error: [],
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    data: [],
+    message: "Listing not found",
+    error: ["Listing not found"],
+  });
 };
