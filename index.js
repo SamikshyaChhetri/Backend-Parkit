4eb838924671e70c2b9c3d019b1b39572c95f192
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-10 18:05:44 +0545

    added a path for review

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import fileUpload from "express-fileupload";
 import loginrouter from "./src/features/auth/login/routes.js";
 import registerRouter from "./src/features/auth/register/route.js";
 import listRouter from "./src/features/listing/route.js";
+import reviewRouter from "./src/features/review/route.js";
 import userRouter from "./src/features/users/route.js";
 const app = express();
 app.use(express.json());
@@ -20,6 +21,7 @@ app.use("/auth", registerRouter);
 app.use("/users", userRouter);
 app.use("/auth", loginrouter);
 app.use("/listing", listRouter);
+app.use("/", reviewRouter);
 app.listen(3333, () => {
   console.log("Server is running at 3333 port");
 });
