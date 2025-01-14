1449bae695a592815e9e77afcce80cb67b5c7570
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-14 23:31:01 +0545

    add endpoint to fetch listings by user owner ID

--- src/features/listing/route.js
+++ src/features/listing/route.js
@@ -3,10 +3,12 @@ import {
   createListingController,
   getlistingsController,
   getSingleListing,
+  getUserListings,
 } from "./controller.js";
 
 const listRouter = Router();
 listRouter.post("/", createListingController);
 listRouter.get("/", getlistingsController);
 listRouter.get("/:id", getSingleListing);
+listRouter.get("/user/:ownerId", getUserListings);
 export default listRouter;
