b6b594fc0a81215f429059c802edf2855366d1d9
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-14 22:54:25 +0545

    add reservation retrieval endpoints and format date in reservation creation

--- src/features/reserve/controller.js
+++ src/features/reserve/controller.js
@@ -1,13 +1,13 @@
+import moment from "moment";
 import { prisma } from "../auth/register/controller.js";
 import { schema } from "./validator.js";
 
 export const createReservationController = async (req, res) => {
   try {
     const { date, listingId, reserverId } = schema.parse(req.body);
-    console.log(data);
     const createdReservation = await prisma.reservation.create({
       data: {
-        date,
+        date: moment(date).toDate(),
         listingId,
         reserverId,
       },
@@ -19,7 +19,53 @@ export const createReservationController = async (req, res) => {
       error: [],
     });
   } catch (error) {
+    console.log(error);
     res.status(500).send({
+      success: false,
+      data: [],
+      message: "Internal server error",
+      error: error,
+    });
+  }
+};
+
+// Fetch all the reservation
+export const getAllReservation = async (req, res) => {
+  try {
+    const allReservations = await prisma.reservation.findMany();
+    console.log(allReservations);
+
+    return res.status(200).send({
+      success: true,
+      data: allReservations,
+      message: "Reservations fetched successfully",
+      error: [],
+    });
+  } catch (error) {
+    return res.status(404).send({
+      success: false,
+      data: [],
+      message: "Internal server error",
+      error: error,
+    });
+  }
+};
+
+//Get a single reservation
+export const getSingleReservation = async (req, res) => {
+  try {
+    const singleReservation = await prisma.reservation.findFirst({
+      where: { id: req.params.id },
+    });
+    console.log(req.params.id);
+    return res.status(200).send({
+      success: true,
+      data: singleReservation,
+      message: "Reservation fetched successfully",
+      error: [],
+    });
+  } catch (error) {
+    return res.status(404).send({
       success: false,
       data: [],
       message: "Internal server error",
--- src/features/reserve/route.js
+++ src/features/reserve/route.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { createReservationController } from "./controller.js";
+import {
+  createReservationController,
+  getAllReservation,
+  getSingleReservation,
+} from "./controller.js";
 
 const reservationRouter = Router();
 reservationRouter.post("/", createReservationController);
+reservationRouter.get("/", getAllReservation);
+reservationRouter.get("/:id", getSingleReservation);
 export default reservationRouter;
