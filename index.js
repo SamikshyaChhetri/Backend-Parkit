20239217d01f091727245f150b0b6c083edf01d5
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-20 17:30:24 +0545

    add dotenv for environment variable management and update CORS origin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import fileUpload from "express-fileupload";
 import loginrouter from "./src/features/auth/login/routes.js";
@@ -11,13 +12,14 @@ import userRouter from "./src/features/users/route.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+dotenv.config();
 app.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
   })
 );
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use("/auth", registerRouter);
 app.use("/users", userRouter);
 app.use("/auth", loginrouter);
