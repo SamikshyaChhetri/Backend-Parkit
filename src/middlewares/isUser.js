2179ac351d3e51c2c71c760e59f3302c448da1ee
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-01-22 23:09:52 +0545

    implement JWT-based user authentication middleware

--- src/middlewares/isUser.js
+++ src/middlewares/isUser.js
@@ -1,4 +1,32 @@
-export const isUser = (req, res, next) => {
-  console.log(JSON.stringify(req.cookies));
-  next();
+import jwt from "jsonwebtoken";
+import { prisma } from "../features/auth/register/controller.js";
+export const isUser = async (req, res, next) => {
+  const cookie = req.cookies.token;
+  const decodedCookie = jwt.decode(cookie);
+  if (!decodedCookie.userid) {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized",
+      data: [],
+      error: [],
+    });
+  }
+  const userId = decodedCookie.userid;
+  const userToken = await prisma.token.findFirst({
+    where: {
+      userId,
+    },
+  });
+  if (cookie == userToken.token) {
+    console.log("Valid user");
+    next();
+  } else {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized",
+      data: [],
+      error: [],
+    });
+  }
+  console.log(JSON.stringify(userToken));
 };
